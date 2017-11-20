0f523933ec59c1334530f4fef869a722219cc0f1
Tarni Sharma <tarni.sharma@gmail.com>
2017-11-20 22:57:26 +0530

    FIrst Commit
    First Commit

---
+++ src/main/java/com/saving/zion/fishonindia/FishonindiaApplication.java
@@ -0,0 +1,12 @@
+package com.saving.zion.fishonindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishonindiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishonindiaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/saving/zion/fishonindia/FishonindiaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saving.zion.fishonindia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FishonindiaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
